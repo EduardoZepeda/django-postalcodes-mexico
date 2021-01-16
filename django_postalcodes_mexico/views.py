265bed60d6de0abc0246f217a60c36d4006dce37
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-01-15 22:14:59 -0600

    Change PostalCodeData view, it now returns error messages

--- django_postalcodes_mexico/views.py
+++ django_postalcodes_mexico/views.py
@@ -2,19 +2,21 @@
 from django.http import JsonResponse
 from django.utils.translation import ugettext_lazy as _
 
+from .forms import PostalCodeForm 
 from .models import (
 	PostalCode,
 )
 
 
 def getPostalCodeData(request, postal_code):
-    postal_codes = PostalCode.objects.filter(d_codigo=postal_code)
-    if postal_codes:
-        data = {'codigoPostal': postal_code, 
+    postalCodeForm = PostalCodeForm({"postal_code": postal_code})
+    if postalCodeForm.is_valid():
+        postal_codes = PostalCode.objects.filter(d_codigo=postal_code)
+        postal_code_data = {'codigoPostal': postal_code, 
                     'municipio': postal_codes[0].D_mnpio,
                     'estado': postal_codes[0].d_estado,
                     'colonias': list(map(lambda x: x.d_asenta, postal_codes))
                     }
-        response = JsonResponse(data)
-    response = {"message": _("The")}
-    return JsonResponse(response, status=400)
+        return JsonResponse(postal_code_data)
+    else:
+        return JsonResponse(postalCodeForm.errors, status=400)
