cc8b7e390fe027a293e282b4375243a2856cb0a9
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-01-17 15:25:08 -0600

    Change model field descriptions according to official docs

--- django_postalcodes_mexico/models.py
+++ django_postalcodes_mexico/models.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -13,21 +12,21 @@ C_TIPO_ASENTA_CHOICES = (('32', 'Villa'), ('37', 'Zona industrial'), ('27', 'Pob
 
 C_ESTADO_CHOICES = (('26', 'Sonora'), ('10', 'Durango'), ('03', 'Baja California Sur'), ('04', 'Campeche'), ('16', 'Michoacán de Ocampo'), ('31', 'Yucatán'), ('19', 'Nuevo León'), ('12', 'Guerrero'), ('11', 'Guanajuato'), ('25', 'Sinaloa'), ('15', 'México'), ('29', 'Tlaxcala'), ('13', 'Hidalgo'), ('08', 'Chihuahua'), ('24', 'San Luis Potosí'), ('27', 'Tabasco'), ('23', 'Quintana Roo'), ('28', 'Tamaulipas'), ('14', 'Jalisco'), ('05', 'Coahuila de Zaragoza'), ('09', 'Ciudad de México'), ('21', 'Puebla'), ('06', 'Colima'), ('02', 'Baja California'), ('32', 'Zacatecas'), ('01', 'Aguascalientes'), ('22', 'Querétaro'), ('30', 'Veracruz de Ignacio de la Llave'), ('20', 'Oaxaca'), ('17', 'Morelos'), ('18', 'Nayarit'), ('07', 'Chiapas'))
 
-
+# Names are obtained from https://www.correosdemexico.gob.mx/SSLServicios/ConsultaCP/imagenes/Descrip.pdf
 
 class PostalCode(TimeStampedModel):
-    d_codigo=models.CharField(_('Codigo Postal'), max_length=5)
-    d_asenta = models.CharField(_('Asentamiento'), max_length=128)
-    D_mnpio = models.CharField(_('Municipio'), max_length=128)
-    d_ciudad = models.CharField(_('Ciudad'), max_length=128, blank=True, null=True)
-    d_CP = models.CharField(_('Código postal'), max_length=128, blank=True, null=True)
-    c_estado = models.CharField(_('Código de estado'), max_length=2, choices=C_ESTADO_CHOICES)
-    c_oficina = models.CharField(_('Código de oficina'), max_length=5)
-    c_tipo_asenta = models.CharField(_('Código de tipo de asentamiento'), max_length=2, choices=C_TIPO_ASENTA_CHOICES)
-    c_mnpio = models.CharField(_('Código de municipio'), max_length=3)
-    id_asenta_cpcons = models.CharField(_('Id Asentamiento'), max_length=4)
-    d_zona = models.CharField(_('Tipo de Zona'), max_length=10, choices=D_ZONA_CHOICES)
-    c_cve_ciudad = models.CharField(_('Asentamiento'), max_length=2, blank=True, null=True)
+    d_codigo=models.CharField(_('Código Postal asentamiento'), max_length=5)
+    d_asenta = models.CharField(_('Nombre asentamiento'), max_length=128)
+    D_mnpio = models.CharField(_('Nombre Municipio (INEGI, Marzo 2013)'), max_length=128)
+    d_ciudad = models.CharField(_('Nombre Ciudad (Catálogo SEPOMEX)'), max_length=128, blank=True, null=True)
+    d_CP = models.CharField(_('Código Postal de la Administración Postal que reparte al asentamiento'), max_length=128, blank=True, null=True)
+    c_estado = models.CharField(_('Clave Entidad (INEGI, Marzo 2013)'), max_length=2, choices=C_ESTADO_CHOICES)
+    c_oficina = models.CharField(_('Código Postal de la Administración Postal que reparte al asentamiento'), max_length=5)
+    c_tipo_asenta = models.CharField(_('Clave Tipo de asentamiento (Catálogo SEPOMEX)'), max_length=2, choices=C_TIPO_ASENTA_CHOICES)
+    c_mnpio = models.CharField(_('Clave Municipio (INEGI, Marzo 2013)'), max_length=3)
+    id_asenta_cpcons = models.CharField(_('Identificador único del asentamiento (nivel municipal)'), max_length=4)
+    d_zona = models.CharField(_('Zona en la que se ubica el asentamiento (Urbano/Rural)'), max_length=10, choices=D_ZONA_CHOICES)
+    c_cve_ciudad = models.CharField(_('Clave Ciudad (Catálogo SEPOMEX)'), max_length=2, blank=True, null=True)
 
     @property
     def c_CP(self):
