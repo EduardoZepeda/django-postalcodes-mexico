a095b5261ccf6323d09bb59b43c0986300412982
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-01-18 17:13:57 -0600

    Start writing tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,19 +7,57 @@ test_django-postalcodes-mexico
 
 Tests for `django-postalcodes-mexico` models module.
 """
+import json
 
 from django.test import TestCase
+from django.urls import reverse
+from mock import patch, MagicMock, Mock
 
-from django_postalcodes_mexico import models
+from django_postalcodes_mexico.models import PostalCode
 
 
 class TestDjango_postalcodes_mexico(TestCase):
 
     def setUp(self):
-        pass
+        PostalCode.objects.get_or_create(
+            d_codigo='01000',
+            d_asenta = 'San Ángel',
+            D_mnpio = 'Álvaro Obregón',
+            d_ciudad = 'Ciudad de México',
+            d_CP = '01001',
+            c_estado = '09',
+            c_oficina = '01001',
+            c_tipo_asenta = '09',
+            c_mnpio = '010',
+            id_asenta_cpcons = '0001',
+            d_zona = 'Urbano',
+            c_cve_ciudad = '01'
+            )
 
-    def test_something(self):
-        pass
+    def test_api_returns_postal_code(self):
+        retrieve_postal_code_data = reverse("django_postalcodes_mexico:get-postal-code-data", args=("01000",))
+        response = self.client.get(retrieve_postal_code_data)
+        self.assertEquals(response.status_code, 200)
+        postal_code_data = json.loads(response.content)
+        self.assertIn("colonias", postal_code_data)
+        self.assertIn("municipio", postal_code_data)
+        self.assertIn("estado", postal_code_data)
+        self.assertIsInstance(postal_code_data, dict)
+
+    def test_api_returns_404_on_not_found_postal_code(self):
+        retrieve_postal_code_data = reverse("django_postalcodes_mexico:get-postal-code-data", args=("99999",))
+        response = self.client.get(retrieve_postal_code_data)
+        self.assertEquals(response.status_code, 404) 
+        postal_code_data = json.loads(response.content)
+        self.assertNotIn("colonia", postal_code_data)
+
+    def test_api_returns_404_on_wrong_input(self):
+        retrieve_postal_code_data = reverse("django_postalcodes_mexico:get-postal-code-data", args=("ABCDE",))
+        response = self.client.get(retrieve_postal_code_data)
+        self.assertEquals(response.status_code, 404) 
+        postal_code_data = json.loads(response.content)
+        self.assertNotIn("colonia", postal_code_data)
+
 
     def tearDown(self):
         pass
