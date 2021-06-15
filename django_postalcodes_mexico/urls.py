e5615967b7e0d838a8893b7beb605caafa182551
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-06-15 12:49:17 -0500

    Replace url functions with pattern in urls
    Since django 2.2 the regex url pattern for deciding the correct url that
    handles the request is replaced with a more easy and simple path. After
    the Django 3.1 release, the url function was deprecated, therefore the
    url function was replaced in the main url file and in tests.

--- django_postalcodes_mexico/urls.py
+++ django_postalcodes_mexico/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
+from django.urls import path
 from django.views.generic import TemplateView
 
 from . import views
@@ -7,14 +7,7 @@ from . import views
 
 app_name = 'django_postalcodes_mexico'
 urlpatterns = [
-    url(
-        regex="^postal-code/(?P<postal_code>\w+)/$",
-        view=views.getPostalCodeData,
-        name='get-postal-code-data',
-    ),
-    url(
-        regex="^postal-code-search/(?P<postal_code>\w+)/$",
-        view=views.searchSimilarPostalCode,
-        name='search-postal-code-data',
-        )
-	]
+    # There are postal codes with leading zeros, hence slug is prefered over int
+    path('postal-code/<slug:postal_code>/', views.getPostalCodeData, name='get-postal-code-data'),
+    path('postal-code-search/<slug:postal_code>/', views.searchSimilarPostalCode, name='search-postal-code-data')
+]
--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('django_postalcodes_mexico.urls', namespace='django_postalcodes_mexico')),
+    path('admin/', admin.site.urls),
+    path('', include('django_postalcodes_mexico.urls', namespace='django_postalcodes_mexico'))
 ]
