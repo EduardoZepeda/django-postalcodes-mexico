245b32499ef734b68c3adebe0742afbd4df949a8
Eduardo Zepeda <eduardozepeda@coffeebytes.com>
2021-11-05 12:01:42 -0600

    Basic refactor of an if else condition

--- django_postalcodes_mexico/views.py
+++ django_postalcodes_mexico/views.py
@@ -14,8 +14,7 @@ def getPostalCodeData(request, postal_code):
         postal_code_data = generateCityAreasByPostalCode(postal_code)
         status=200 if postal_code_data else 404       
         return JsonResponse(postal_code_data, status=status)
-    else:
-        return JsonResponse(postalCodeForm.errors, status=400)
+    return JsonResponse(postalCodeForm.errors, status=400)
 
 def searchSimilarPostalCode(request, postal_code):
     postalCodeForm = PostalCodeSearchForm({"postal_code": postal_code})
@@ -23,5 +22,4 @@ def searchSimilarPostalCode(request, postal_code):
         postal_codes_list = searchPostalCode(postal_code)
         status=200 if postal_codes_list else 404       
         return JsonResponse(postal_codes_list, status=status)
-    else:
-        return JsonResponse(postalCodeForm.errors, status=400)    
+    return JsonResponse(postalCodeForm.errors, status=400)    
