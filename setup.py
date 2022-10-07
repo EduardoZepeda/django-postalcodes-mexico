fc1f6dceb61166200c5d4ee903e7681e311b8084
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-10-06 19:54:13 -0500

    Update publish commands on setup.py

--- setup.py
+++ setup.py
@@ -31,8 +31,8 @@ if sys.argv[-1] == 'publish':
     except ImportError:
         print('Wheel library missing. Please run "pip install wheel"')
         sys.exit()
-    os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
+    os.system('python setup.py bdist_wheel')
+    os.system('twine upload dist/*')
     sys.exit()
 
 if sys.argv[-1] == 'tag':
