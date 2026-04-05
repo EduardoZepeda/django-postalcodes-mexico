130c42d11237dc31479bd3348b33745499d1028c
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2026-04-04 21:34:23 -0600

    Update for django 6.0

--- setup.py
+++ setup.py
@@ -14,64 +14,65 @@ def get_version(*file_paths):
     """Retrieves the version from django_postalcodes_mexico/__init__.py"""
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
     version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
+    raise RuntimeError("Unable to find version string.")
 
 
 version = get_version("django_postalcodes_mexico", "__init__.py")
 
 
-if sys.argv[-1] == 'publish':
+if sys.argv[-1] == "publish":
     try:
         import wheel
+
         print("Wheel version: ", wheel.__version__)
     except ImportError:
         print('Wheel library missing. Please run "pip install wheel"')
         sys.exit()
-    os.system('python setup.py bdist_wheel')
-    os.system('twine upload --repository django-postalcodes-mexico dist/*')
+    os.system("python setup.py bdist_wheel")
+    os.system("twine upload --repository django-postalcodes-mexico dist/*")
     sys.exit()
 
-if sys.argv[-1] == 'tag':
+if sys.argv[-1] == "tag":
     print("Tagging the version on git:")
     os.system("git tag -a %s -m 'version %s'" % (version, version))
     os.system("git push --tags")
     sys.exit()
 
-readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
-requirements = open('requirements.txt').readlines()
+readme = open("README.rst").read()
+history = open("HISTORY.rst").read().replace(".. :changelog:", "")
+requirements = open("requirements.txt").readlines()
 
 setup(
-    name='django-postalcodes-mexico',
+    name="django-postalcodes-mexico",
     version=version,
     description="""A Django Package for handling Mexico's postal codes""",
-    long_description=readme + '\n\n' + history,
-    author='Eduardo Zepeda',
-    author_email='eduardozepeda@coffeebytes.dev',
-    url='https://github.com/EduardoZepeda/django-postalcodes-mexico',
+    long_description=readme + "\n\n" + history,
+    author="Eduardo Zepeda",
+    author_email="eduardozepeda@coffeebytes.dev",
+    url="https://github.com/EduardoZepeda/django-postalcodes-mexico",
     packages=[
-        'django_postalcodes_mexico',
+        "django_postalcodes_mexico",
     ],
     include_package_data=True,
     install_requires=requirements,
     license="MIT",
     zip_safe=False,
-    keywords='django-postalcodes-mexico',
+    keywords="django-postalcodes-mexico",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Django :: 3.1',
-        'Framework :: Django :: 3.2',
-        'Framework :: Django :: 4.1',
-        'Framework :: Django :: 5.0',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
+        "Development Status :: 4 - Beta",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.2",
+        "Framework :: Django :: 6.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
 )
--- tests/test_models.py
+++ tests/test_models.py
@@ -7,6 +7,7 @@ test_django-postalcodes-mexico
 
 Tests for `django-postalcodes-mexico` models module.
 """
+
 import json
 
 from django.test import TestCase
@@ -17,25 +18,33 @@ from django_postalcodes_mexico.models import PostalCode
 
 
 class TestDjango_postalcodes_mexico(TestCase):
-
     def setUp(self):
         PostalCode.objects.get_or_create(
-            d_codigo='01000',
-            d_asenta='San Ángel',
-            D_mnpio='Álvaro Obregón',
-            d_ciudad='Ciudad de México',
-            d_CP='01001',
-            c_estado='09',
-            c_oficina='01001',
-            c_tipo_asenta='09',
-            c_mnpio='010',
-            id_asenta_cpcons='0001',
-            d_zona='Urbano',
-            c_cve_ciudad='01'
+            d_codigo="01000",
+            d_asenta="San Ángel",
+            D_mnpio="Álvaro Obregón",
+            d_ciudad="Ciudad de México",
+            d_CP="01001",
+            c_estado="09",
+            c_oficina="01001",
+            c_tipo_asenta="09",
+            c_mnpio="010",
+            id_asenta_cpcons="0001",
+            d_zona="Urbano",
+            c_cve_ciudad="01",
         )
+        # assertEquals was deprecated in Python 3.12, since we need it to run the previous tests
+        # we assign it to assertEquals if it's remove
+        try:
+            # This will throw an error in Python 3.12
+            self.deprecated = self.assertEquals
+        except AttributeError:
+            self.assertEquals = self.assertEqual
 
     def test_api_returns_postal_code_data(self):
-        retrieve_postal_code_data = reverse("django_postalcodes_mexico:get-postal-code-data", args=("01000",))
+        retrieve_postal_code_data = reverse(
+            "django_postalcodes_mexico:get-postal-code-data", args=("01000",)
+        )
         response = self.client.get(retrieve_postal_code_data)
         self.assertEquals(response.status_code, 200)
         postal_code_data = json.loads(response.content)
@@ -43,17 +52,21 @@ class TestDjango_postalcodes_mexico(TestCase):
         self.assertIn("colonias", postal_code_data)
         self.assertIn("municipio", postal_code_data)
         self.assertIn("estado", postal_code_data)
-        self.assertIsInstance(postal_code_data.get('colonias'), list)
+        self.assertIsInstance(postal_code_data.get("colonias"), list)
 
     def test_api_returns_404_on_not_found_postal_code(self):
-        retrieve_postal_code_data = reverse("django_postalcodes_mexico:get-postal-code-data", args=("99999",))
+        retrieve_postal_code_data = reverse(
+            "django_postalcodes_mexico:get-postal-code-data", args=("99999",)
+        )
         response = self.client.get(retrieve_postal_code_data)
         self.assertEquals(response.status_code, 404)
         postal_code_data = json.loads(response.content)
         self.assertEquals(len(postal_code_data), 0)
 
     def test_api_returns_400_on_alpha_input(self):
-        retrieve_postal_code_data = reverse("django_postalcodes_mexico:get-postal-code-data", args=("ABCDE",))
+        retrieve_postal_code_data = reverse(
+            "django_postalcodes_mexico:get-postal-code-data", args=("ABCDE",)
+        )
         response = self.client.get(retrieve_postal_code_data)
         self.assertEquals(response.status_code, 400)
         postal_code_data = json.loads(response.content)
@@ -61,7 +74,9 @@ class TestDjango_postalcodes_mexico(TestCase):
         self.assertIn("postal_code", postal_code_data)
 
     def test_api_returns_400_on_long_digit_input(self):
-        retrieve_postal_code_data = reverse("django_postalcodes_mexico:get-postal-code-data", args=("999999999999",))
+        retrieve_postal_code_data = reverse(
+            "django_postalcodes_mexico:get-postal-code-data", args=("999999999999",)
+        )
         response = self.client.get(retrieve_postal_code_data)
         self.assertEquals(response.status_code, 400)
         postal_code_data = json.loads(response.content)
